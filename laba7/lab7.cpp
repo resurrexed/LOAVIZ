8b0d40d9c678cb53b7af3df2903870eb94ea2f0e
resurrexed <noreasee@gmail.com>
2024-11-06 10:52:26 +0300

    lab7 zadaniya

--- laba7/lab7.cpp
+++ laba7/lab7.cpp
@@ -5,25 +5,23 @@
 #include <stack>
 #include <vector>
 using namespace std;
-#include <iostream>
-#include <vector>
-#include <queue>
-
 class Matrix {
 private:
   int size;
-  vector<vector<int>> matrix;
-  vector<bool> NUM; 
+  vector<vector<int>> matrix; 
 public:
-  Matrix(int size) : size(size), matrix(size, std::vector<int>(size)), NUM(size, false) {}
-  void GenerateMatrix() 
+  vector<bool> NUM;
+  vector<bool> visited;
+  Matrix(int size) : size(size), matrix(size, std::vector<int>(size)), NUM(size, false), visited(size, false) {}
+  void GenerateMatrix(int random) 
   {
     for(int i=0;i<size;i++)
     {
       matrix[i][i]=0;
       for(int j=i+1;j<size;j++)
       {
-        matrix[i][j]=rand() % 2;
+        if(random < rand() % 101) matrix[i][j]=0;
+        else matrix[i][j]=1;
         matrix[j][i]=matrix[i][j];
       }
     }
@@ -44,6 +42,7 @@ public:
       if (!NUM[i]) {
         Proxod(i);
       }
+      break;
     }
   }
   void Proxod(int v) {
@@ -58,7 +57,6 @@ public:
   }
   void DFS(int start)
   {
-    vector<bool> visited(size, false);
     stack<int> st;
     
     // Помещаем начальную вершину в стек
@@ -96,14 +94,27 @@ public:
 int main()
 {
   srand(time(NULL));
-  int size;
-  cout<<"Enter number of value ";
-  cin>>size;
+  int size, start,random;
+  cout<<"Enter number of value and chance to generate the rib in % ";
+  cin>>size>>random;
   Matrix graph(size);
-  graph.GenerateMatrix();
+  graph.GenerateMatrix(random);
   graph.PrintMatrix();
-  graph.Check_false();
   cout<<endl;
-  graph.DFS(0);
+  cout<<"DFS recursive"<<endl;
+  graph.Check_false();
+  cout<<'\n'<<"Enter vertex for DFS"<<endl;
+  cin>>start;
+  cout<<"DFS without recursive"<<endl;
+  graph.DFS(start-1);
+  cout<<endl;
+  for(int i=0;i<size;i++)
+  {
+    if(graph.visited[i]==false) 
+    {
+      cout<<endl;
+      graph.DFS(i);
+    }
+  }
   return 0;
 }
